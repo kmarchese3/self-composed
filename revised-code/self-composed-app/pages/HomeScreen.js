b53b5c1a379bfa0c9a7674740a573ed41feab1b5
Giana <gabbas@villanova.edu>
2024-04-09 14:40:20 -0400

    Changed text sizing

--- revised-code/self-composed-app/pages/HomeScreen.js
+++ revised-code/self-composed-app/pages/HomeScreen.js
@@ -4,19 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{fontWeight: 'bold'}}>hello, user</Text>
+      <Text style={{fontWeight: 'bold', fontSize: '8vmin'}}>hello, user</Text>
       <StatusBar style="auto" />
 
       <View style={styles.container2}>
-        <Text>how are you feeling today?</Text>
-        <Text>(mood buttons go here)</Text>
+        <Text style={styles.containertext}>how are you feeling today?</Text>
+        <Text style={styles.containersubtext}>(mood buttons go here)</Text>
       </View>
       
       <View style={styles.container2}>
-        <Text>journaling prompt</Text>
-        <Text>(prompt goes here)</Text>
-        <Text>daily tasks</Text>
-        <Text>(tasks go here)</Text>
+        <Text style={styles.containertext}>journaling prompt</Text>
+        <Text style={styles.containersubtext}>(prompt goes here)</Text>
+        <Text style={styles.containertext}>daily tasks</Text>
+        <Text style={styles.containersubtext}>(tasks go here)</Text>
       </View>
     </View>
   );
@@ -29,11 +29,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#D8FBBD',
     alignItems: 'left',
     justifyContent: 'top',
-    padding: 20
+    padding: 20,
+    fontFamily: 'Inter'
   },
   container2: {
     backgroundColor: '#FFFFFF',
     marginTop: 20,
     padding: 20,
+  },
+  containertext: {
+    fontSize: '3vmin',
+    lineHeight: '4vmin'
+  },
+  containersubtext: {
+    fontSize: '2.5vmin',
+    lineHeight: '3.5vmin'
   }
 });
