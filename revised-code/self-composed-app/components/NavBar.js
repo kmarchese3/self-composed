3e9c8d1823a9cc7a4043798a1671cdeeb3ff8e28
Giana <gabbas@villanova.edu>
2024-04-23 15:14:39 -0400

    Fixed navigation on mobile

--- revised-code/self-composed-app/components/NavBar.js
+++ revised-code/self-composed-app/components/NavBar.js
@@ -1,14 +1,20 @@
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, Pressable } from "react-native"
 import { useNavigation } from "@react-navigation/native";
 
 export const NavBar = () => {
     const navigation = useNavigation()
     return (
         <View style={styles.navbarcontainer}>
-            <Text style={styles.navbartext} onClick={() => navigation.navigate('Home')}>home</Text>
+            <Pressable onPress={() => navigation.navigate('Home')}>
+                <Text style={styles.navbartext} onClick={() => navigation.navigate('Home')}>home</Text>
+            </Pressable>
             <Text style={styles.navbartext}>journaling</Text>
-            <Text style={styles.navbartext} onClick={() => navigation.navigate('SelfCare')}>self-care</Text>
-            <Text style={styles.navbartext} onClick={() => navigation.navigate('Profile')}>profile</Text>
+            <Pressable onPress={() => navigation.navigate('SelfCare')}>
+                <Text style={styles.navbartext} onClick={() => navigation.navigate('SelfCare')}>self-care</Text>
+            </Pressable>
+            <Pressable onPress={() => navigation.navigate('Profile')}>
+                <Text style={styles.navbartext} onClick={() => navigation.navigate('Profile')}>profile</Text>
+            </Pressable>
         </View>
     )
 }
